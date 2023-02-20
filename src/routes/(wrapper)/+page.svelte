be84e4fd202b8a1c3a66facba452fe7ebb08fe01
Sea Grapes <natuloid@gmail.com>
2023-02-19 17:15:06 -0800

    .

---
+++ src/routes/(wrapper)/+page.svelte
@@ -0,0 +1,37 @@
+
+<script>
+</script>
+
+<article>
+  <header>
+    <h1>Re:
+      <span class='rotate'>imagine</span>
+      <span class='rotate'>design</span>
+      <span class='rotate'>discover</span>
+    </h1>
+    <h1>WebDev</h1>
+  </header>
+  <section>
+  </section>
+</article>
+
+<style>
+
+  @keyframes rotate {
+    0% {transform: rotateY(0deg)}
+    50% {transform: rotateY(-90deg)}
+  }
+
+  .rotate {
+    display: inline-block;
+    animation: 4s rotate;
+  }
+
+  header {
+    margin: 4rem 0;
+  }
+
+  article {
+    max-width: 75vw;
+  }
+</style>
