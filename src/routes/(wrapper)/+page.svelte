02e87fbd2a53015f733bab7a337818e45675803b
Sea Grapes <natuloid@gmail.com>
2023-06-11 12:59:18 -0800

    bru

--- src/routes/(wrapper)/+page.svelte
+++ src/routes/(wrapper)/+page.svelte
@@ -4,6 +4,8 @@
 
 <header>
   <cs>Portfolio();</cs>
+  <input type="checkbox" id="nav-toggle">
+  <label for="nav-toggle"></label>
   <nav>
     <a href="#">Home</a>
     <a href="#">Blog</a>
@@ -23,32 +25,78 @@
 
 <style>
 
+input {
+
+}
+
+cs {
+  font-family: Consolas;
+}
+
+header cs {
+  display: block;
+  text-align: center;
+}
+
+.container {
+  max-width: 50rem;
+  margin: 0 auto;
+  padding: 0 2rem;
+}
+
+section {
+  padding: 3.5rem 0;
+}
+
+header {
+  padding: 2rem;
+}
+
+nav {
+  text-align: left;
+  /* display: grid; */
+  transform: scale(1, 0);
+  transform-origin: top;
+  transition: transform 400ms ease-in-out;
+}
+
+nav a {
+  margin-right: 1rem;
+  display: block;
+}
+
+
+#nav-toggle:checked ~ nav {
+  transform: scale(1, 1);
+}
+
+#nav-toggle + label {
+  background-image: url('logo.svg');
+}
+
+
+@media screen and (min-width: 800px) {
   cs {
-    font-family: Consolas;
-  }
-
-  .container {
-    max-width: 50rem;
-    margin: 0 auto;
-    padding: 0 2rem;
-  }
-
-  section {
-    padding: 3.5rem 0;
+    display: inline;
   }
 
   header {
     display: flex;
     justify-content: space-between;
-    padding: 2rem;
   }
 
   nav {
-    text-align: left;
-    /* display: grid; */
+    transition: none;
+    transform: scale(1, 1);
   }
 
   nav a {
-    margin-right: 1rem;
+    display: inline;
   }
+
+  #nav-toggle {
+    display: none;
+  }
+}
+
 </style>
