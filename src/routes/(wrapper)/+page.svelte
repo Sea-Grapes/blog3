b231a82bee20179d6238a211115d15d5ae45fa21
Sea Grapes <natuloid@gmail.com>
2023-06-11 16:20:26 -0800

    .

--- src/routes/(wrapper)/+page.svelte
+++ src/routes/(wrapper)/+page.svelte
@@ -1,29 +1,9 @@
-
 <script>
-    import Nav from "./header/Nav.svelte";
-
+import Header from "./header/Header.svelte";
 </script>
 
-<header class="ns">
-  <input type="checkbox" id="nav-toggle">
-  <cs>
-  <label for="nav-toggle"><span class="material-symbols-rounded">menu</span></label>
-    Portfolio();
-  </cs>
-  <nav>
-    <Nav url='/' label='Home'/>
-    <Nav url='/' label='Blog'/>
-    <Nav url='/' label='Projects'/>
-    <Nav url='/' label='About'/>
+<Header/>
 
-    
-    
-    <!-- <a href="#">Home</a>
-    <a href="#">Blog</a>
-    <a href="#">Projects</a>
-    <a href="#">About</a> -->
-  </nav>
-</header>
 <main>
   <section>
     <div class="container">
@@ -32,24 +12,7 @@
   </section>
 </main>
 
-
-
 <style>
-
-input {
-
-}
-
-cs {
-  font-family: Consolas;
-}
-
-header cs {
-  display: grid;
-  place-items: center;
-  height: 4rem;
-}
-
 .container {
   max-width: 50rem;
   margin: 0 auto;
@@ -59,69 +22,4 @@ header cs {
 section {
   padding: 3.5rem 0;
 }
-
-header {
-  padding: 0 2rem;
-}
-
-nav {
-  text-align: left;
-  transform: scale(1, 0);
-  transform-origin: top;
-  transition: transform 400ms ease-in-out;
-}
-
-nav a {
-  display: block;
-}
-
-
-#nav-toggle {
-  display: none;
-}
-
-
-#nav-toggle:checked ~ nav {
-  transform: scale(1, 1);
-}
-
-label[for='nav-toggle'] {
-  display: flex;
-  position: absolute;
-  left: 2rem;
-  font-size: 2em;
-}
-
-@media screen and (min-width: 800px) {
-  cs {
-    display: inline;
-  }
-
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 4rem;
-  }
-
-  nav {
-    transition: none;
-    transform: scale(1, 1);
-    display: flex;
-    align-items: center;
-    height: 100%;
-  }
-
-  nav a {
-    display: flex;
-    padding: 0 1rem;
-    height: 100%;
-    align-items: center;
-  }
-
-  label[for='nav-toggle'] {
-    display: none;
-  }
-}
-
 </style>
---
+++ src/routes/(wrapper)/header/Header.svelte
@@ -0,0 +1,110 @@
+
+<script>
+    import Nav from "./Nav.svelte";
+</script>
+
+<header class="ns">
+  <input type="checkbox" id="nav-toggle">
+  <cs>
+  <label for="nav-toggle"><span class="material-symbols-rounded">menu</span></label>
+    Portfolio();
+  </cs>
+  <nav>
+    <Nav url='/' label='Home'/>
+    <Nav url='/' label='Blog'/>
+    <Nav url='/' label='Projects'/>
+    <Nav url='/' label='About'/>
+
+    
+    
+    <!-- <a href="#">Home</a>
+    <a href="#">Blog</a>
+    <a href="#">Projects</a>
+    <a href="#">About</a> -->
+  </nav>
+</header>
+
+
+
+<style>
+
+input {
+
+}
+
+cs {
+  font-family: Consolas;
+}
+
+header cs {
+  display: grid;
+  place-items: center;
+  height: 4rem;
+}
+
+
+header {
+  padding: 0 2rem;
+}
+
+nav {
+  text-align: left;
+  transform: scale(1, 0);
+  transform-origin: top;
+  transition: transform 400ms ease-in-out;
+}
+
+nav a {
+  display: block;
+}
+
+
+#nav-toggle {
+  display: none;
+}
+
+
+#nav-toggle:checked ~ nav {
+  transform: scale(1, 1);
+}
+
+label[for='nav-toggle'] {
+  display: flex;
+  position: absolute;
+  left: 2rem;
+  font-size: 2em;
+}
+
+@media screen and (min-width: 800px) {
+  cs {
+    display: inline;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 4rem;
+  }
+
+  nav {
+    transition: none;
+    transform: scale(1, 1);
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+
+  nav a {
+    display: flex;
+    padding: 0 1rem;
+    height: 100%;
+    align-items: center;
+  }
+
+  label[for='nav-toggle'] {
+    display: none;
+  }
+}
+
+</style>
