a42449921c8e19fc071e60c83fdf22490bf5ea8d
Sea Grapes <natuloid@gmail.com>
2023-06-24 14:59:40 -0800

    .

--- src/routes/(wrapper)/about/+page.svelte
+++ src/routes/(wrapper)/about/+page.svelte
@@ -2,28 +2,7 @@
   <section>
     <div class="container dark">
       <h1>About</h1>
-      Hi, I'm 13907, a passionate tech enthusiast 
+      Hi, I'm E, a passionate tech enthusiast 
     </div>
   </section>
 </main>
-
-<style>
-
-:global(body) {
-
-}
-
-.container {
-  max-width: 50rem;
-  margin: 0 auto;
-  padding: 0 2rem;
-}
-
-.dark {
-  
-}
-
-section {
-  padding: 3.5rem 0;
-}
-</style>
--- src/routes/(wrapper)/projects/+page.svelte
+++ src/routes/(wrapper)/projects/+page.svelte
@@ -6,15 +6,3 @@
     </div>
   </section>
 </main>
-
-<style>
-.container {
-  max-width: 50rem;
-  margin: 0 auto;
-  padding: 0 2rem;
-}
-
-section {
-  padding: 3.5rem 0;
-}
-</style>
