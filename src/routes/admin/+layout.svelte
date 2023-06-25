3b5fcbe65b46bc4d0e6a2df695806a4561bcf450
Sea Grapes <natuloid@gmail.com>
2023-06-24 16:33:14 -0800

    .

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -4,6 +4,7 @@
     <a href="dashboard"><span class="material-symbols-rounded">grid_view</span>Dashboard</a>
     <a href="blog"><span class="material-symbols-rounded">article</span>Blog</a>
     <a href="projects"><span class="material-symbols-rounded">folder</span>Projects</a>
+    <a href="projects"><span class="material-symbols-rounded">comment</span>Comments</a>
   </div>
   <div>
     <header>Banner thing</header>
@@ -20,19 +21,18 @@
     min-height: 100vh;
   }
   
+  .body {
+    padding: 0 2rem;
+  }
+  
   header {
     padding: 1.2rem 2rem;
     border-bottom: 1px solid var(--slate-200);
     margin-bottom: 2rem;
   }
-  
 
   .left {
     background-color: var(--slate-50);
-  }
-
-  .body {
-    padding: 0 2rem;
   }
 
   a {
@@ -44,8 +44,5 @@
   a span {
     margin-right: 0.5em;
   }
-
-  .base:last-child {
-
-  }
+  
 </style>
